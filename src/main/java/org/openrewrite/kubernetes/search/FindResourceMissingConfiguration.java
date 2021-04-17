d99902e794d3581016fe0a120c52c7d356bee3b5
Jonathan Schneider <jkschneider@gmail.com>
2021-04-17 16:40:13 -0700

    License headers, add FindResourceMissingConfiguration

---
+++ src/main/java/org/openrewrite/kubernetes/search/FindResourceMissingConfiguration.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.kubernetes.search;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.marker.RecipeSearchResult;
+import org.openrewrite.yaml.YamlIsoVisitor;
+import org.openrewrite.yaml.search.FindKey;
+import org.openrewrite.yaml.tree.Yaml;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class FindResourceMissingConfiguration extends Recipe {
+    @Option(displayName = "Resource kind",
+            description = "The Kubernetes resource type to search on.",
+            example = "Pod")
+    String resourceKind;
+
+    @Option(displayName = "Configuration path",
+            description = "An XPath expression to locate Kubernetes configuration.",
+            example = "/spec/containers/livenessProbe")
+    String configurationPath;
+
+    @Override
+    public String getDisplayName() {
+        return "Missing configuration";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Find Kubernetes resources with missing configuration.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new YamlIsoVisitor<ExecutionContext>() {
+            @Override
+            public Yaml.Document visitDocument(Yaml.Document document, ExecutionContext context) {
+                return FindKey.find(document, configurationPath).isEmpty() ?
+                        document.withMarker(new RecipeSearchResult(FindResourceMissingConfiguration.this)) :
+                        document;
+            }
+        };
+    }
+}
--- src/main/java/org/openrewrite/kubernetes/search/package-info.java
+++ src/main/java/org/openrewrite/kubernetes/search/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2020 the original author or authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,29 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openrewrite.kubernetes.tree;
+@NonNullApi
+@NonNullFields
+package org.openrewrite.kubernetes.search;
 
-import lombok.Value;
-import org.openrewrite.internal.lang.Nullable;
-import org.openrewrite.marker.Marker;
-
-import java.util.Map;
-
-@Value
-public class KubernetesResource implements Marker {
-    String apiVersion;
-    String kind;
-    Metadata metadata;
-
-    @Value
-    public static class Metadata {
-        @Nullable
-        String name;
-
-        @Nullable
-        Map<String, String> annotations;
-
-        @Nullable
-        Map<String, String> labels;
-    }
-}
+import org.openrewrite.internal.lang.NonNullApi;
+import org.openrewrite.internal.lang.NonNullFields;
