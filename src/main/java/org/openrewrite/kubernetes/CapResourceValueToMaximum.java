28a2eedfe1d09cadf5c9a1e3a6bd0b0b449797b6
Jonathan Schneider <jkschneider@gmail.com>
2021-07-01 07:41:18 -0700

    Polish

--- src/main/java/org/openrewrite/kubernetes/CapResourceValueToMaximum.java
+++ src/main/java/org/openrewrite/kubernetes/CapResourceValueToMaximum.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.kubernetes;
 
 import lombok.EqualsAndHashCode;
@@ -7,8 +22,9 @@ import org.openrewrite.Option;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.kubernetes.util.ResourceValue;
-import org.openrewrite.yaml.MergeValueVisitor;
 import org.openrewrite.yaml.XPathMatcher;
+import org.openrewrite.yaml.YamlIsoVisitor;
+import org.openrewrite.yaml.tree.Yaml;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
@@ -18,10 +34,12 @@ public class CapResourceValueToMaximum extends Recipe {
             description = "The type of resource to search for.",
             example = "limits")
     String resourceValueType;
+
     @Option(displayName = "Resource type",
             description = "The type of resource value to search for.",
             example = "memory")
     String resourceType;
+
     @Option(displayName = "Resource limit",
             description = "The resource maximum to search for to find resources that request more than the maximum.",
             example = "2Gi")
@@ -40,14 +58,20 @@ public class CapResourceValueToMaximum extends Recipe {
     @Override
     protected TreeVisitor<?, ExecutionContext> getVisitor() {
         ResourceValue max = ResourceValue.parseResourceString(resourceLimit);
+        XPathMatcher resourceMatcher = new XPathMatcher("spec/containers/resources/" + resourceValueType + "/" + resourceType);
 
-        return new MergeValueVisitor<>(
-                "spec/containers/resources/" + resourceValueType + "/" + resourceType,
-                new ExceedsResourceValue(max),
-                (s, ctx) -> {
-                    ResourceValue rv = ResourceValue.parseResourceString(s.getValue());
-                    return s.withValue(max.convertTo(rv.getUnit()).toString());
-                });
+        return new YamlIsoVisitor<ExecutionContext>() {
+            @Override
+            public Yaml.Mapping.Entry visitMappingEntry(Yaml.Mapping.Entry entry, ExecutionContext executionContext) {
+                if (resourceMatcher.matches(getCursor()) && entry.getValue() instanceof Yaml.Scalar) {
+                    Yaml.Scalar scalarValue = (Yaml.Scalar) entry.getValue();
+                    ResourceValue rv = ResourceValue.parseResourceString(scalarValue.getValue());
+                    if (rv.getAbsoluteValue() > max.getAbsoluteValue()) {
+                        return entry.withValue(scalarValue.withValue(max.convertTo(rv.getUnit()).toString()));
+                    }
+                }
+                return super.visitMappingEntry(entry, executionContext);
+            }
+        };
     }
-
 }
--- src/main/java/org/openrewrite/kubernetes/search/FindExceedsResourceValue.java
+++ src/main/java/org/openrewrite/kubernetes/search/FindExceedsResourceValue.java
@@ -1,12 +1,30 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openrewrite.kubernetes.search;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.openrewrite.*;
-import org.openrewrite.kubernetes.ExceedsResourceValue;
 import org.openrewrite.kubernetes.util.ResourceValue;
-import org.openrewrite.yaml.MergeValueVisitor;
+import org.openrewrite.yaml.XPathMatcher;
+import org.openrewrite.yaml.YamlIsoVisitor;
 import org.openrewrite.yaml.search.YamlSearchResult;
+import org.openrewrite.yaml.tree.Yaml;
+
+import static org.openrewrite.Tree.randomId;
 
 @Value
 @EqualsAndHashCode(callSuper = true)
@@ -39,13 +57,23 @@ public class FindExceedsResourceValue extends Recipe {
     @Override
     protected TreeVisitor<?, ExecutionContext> getVisitor() {
         ResourceValue max = ResourceValue.parseResourceString(resourceLimit);
+        XPathMatcher resourceMatcher = new XPathMatcher("spec/containers/resources/" + resourceValueType + "/" + resourceType);
 
-        return new MergeValueVisitor<>(
-                "spec/containers/resources/" + resourceValueType + "/" + resourceType,
-                new ExceedsResourceValue(max),
-                (s, ctx) -> s.withMarkers(s.getMarkers().addIfAbsent(new YamlSearchResult(Tree.randomId(),
-                        FindExceedsResourceValue.this,
-                        "exceeds maximum of " + max))));
+        return new YamlIsoVisitor<ExecutionContext>() {
+            @Override
+            public Yaml.Mapping.Entry visitMappingEntry(Yaml.Mapping.Entry entry, ExecutionContext executionContext) {
+                if (resourceMatcher.matches(getCursor()) && entry.getValue() instanceof Yaml.Scalar) {
+                    Yaml.Scalar scalarValue = (Yaml.Scalar) entry.getValue();
+                    ResourceValue rv = ResourceValue.parseResourceString(scalarValue.getValue());
+                    if (rv.getAbsoluteValue() > max.getAbsoluteValue()) {
+                        return entry.withValue(scalarValue.withMarkers(scalarValue.getMarkers().addIfAbsent(new YamlSearchResult(randomId(),
+                                FindExceedsResourceValue.this,
+                                "exceeds maximum of " + max))));
+                    }
+                }
+                return super.visitMappingEntry(entry, executionContext);
+            }
+        };
     }
 
 }
