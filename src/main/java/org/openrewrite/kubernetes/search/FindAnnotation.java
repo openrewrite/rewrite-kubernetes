7673cf39c4efc3a1c2c9058fc062a92b091cf082
Jon Brisbin <jon@moderne.io>
2021-07-09 11:05:58 -0500

    Find annotations by presence and value

---
+++ src/main/java/org/openrewrite/kubernetes/search/FindAnnotation.java
@@ -0,0 +1,71 @@
+/*
+ *  Copyright 2021 the original author or authors.
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openrewrite.kubernetes.search;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.*;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.yaml.search.YamlSearchResult;
+import org.openrewrite.yaml.tree.Yaml;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+public class FindAnnotation extends Recipe {
+
+    @Option(displayName = "Annotation name",
+            description = "The name of the annotation to search for the existence of.",
+            example = "mycompany.io/annotation")
+    String annotationName;
+    @Option(displayName = "Value regex",
+            description = "An optional regex that will find values that match.",
+            example = "value.*",
+            required = false)
+    @Nullable
+    String valueRegex;
+
+    @Override
+    public String getDisplayName() {
+        return "Find annotation";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Find annotations that optionally match a given regex.";
+    }
+
+    @Override
+    protected TreeVisitor<?, ExecutionContext> getVisitor() {
+        YamlSearchResult found = new YamlSearchResult(this, "found:" + annotationName);
+        YamlSearchResult valid = new YamlSearchResult(this, "found:" + valueRegex);
+
+        return new ValidatingMappingEntryVisitor("//metadata/annotations", annotationName, valueRegex) {
+            @Override
+            public Yaml.Mapping.Entry visitFoundEntry(Yaml.Mapping.Entry entry, Cursor parent, ExecutionContext ctx) {
+                parent.putMessageOnFirstEnclosing(Yaml.Mapping.Entry.class, MESSAGE_KEY, found);
+                return entry;
+            }
+
+            @Override
+            public Yaml.Mapping.Entry visitValidEntry(Yaml.Mapping.Entry entry, Cursor parent, ExecutionContext ctx) {
+                parent.putMessageOnFirstEnclosing(Yaml.Mapping.Entry.class, MESSAGE_KEY, valid);
+                return super.visitInvalidEntry(entry, parent, ctx);
+            }
+        };
+    }
+
+}
---
+++ src/test/kotlin/org/openrewrite/kubernetes/search/FindMissingAnnotationTest.kt
@@ -0,0 +1,156 @@
+/*
+ *  Copyright 2021 the original author or authors.
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openrewrite.kubernetes.search
+
+import org.junit.jupiter.api.Test
+import org.openrewrite.kubernetes.KubernetesRecipeTest
+
+class FindMissingAnnotationTest : KubernetesRecipeTest {
+
+    @Test
+    fun `must find missing annotation`() = assertChanged(
+        recipe = FindMissingAnnotation(
+            "mycompany.io/annotation",
+            null
+        ),
+        before = """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod1
+              annotations:
+                mycompany.io/something: "hasvalue"
+            ---
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod2
+              annotations:
+                mycompany.io/annotation: "novalue"
+            ---
+            apiVersion: apps/v1
+            kind: Deployment
+            spec:
+                template:
+                    spec:
+                        metadata:
+                            annotations:
+                                mycompany.io/something: "hasvalue"
+                        containers:            
+                        - name: app
+                          image: repo/app:latest
+                        - name: sidecar
+                          image: repo/sidecar:dev
+        """.trimIndent(),
+        after = """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod1
+              ~~(missing:mycompany.io/annotation)~~>annotations:
+                mycompany.io/something: "hasvalue"
+            ---
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod2
+              annotations:
+                mycompany.io/annotation: "novalue"
+            ---
+            apiVersion: apps/v1
+            kind: Deployment
+            spec:
+                template:
+                    spec:
+                        metadata:
+                            ~~(missing:mycompany.io/annotation)~~>annotations:
+                                mycompany.io/something: "hasvalue"
+                        containers:            
+                        - name: app
+                          image: repo/app:latest
+                        - name: sidecar
+                          image: repo/sidecar:dev
+        """.trimIndent()
+    )
+
+    @Test
+    fun `must find invalid annotation`() = assertChanged(
+        recipe = FindMissingAnnotation(
+            "mycompany.io/annotation",
+            "has(.*)"
+        ),
+        before = """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod1
+              annotations:
+                mycompany.io/annotation: "hasvalue"
+            ---
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod2
+              annotations:
+                mycompany.io/annotation: "novalue"
+            ---
+            apiVersion: apps/v1
+            kind: Deployment
+            spec:
+                template:
+                    spec:
+                        metadata:
+                            annotations:
+                                mycompany.io/annotation: "hasvalue"
+                        containers:            
+                        - name: app
+                          image: repo/app:latest
+                        - name: sidecar
+                          image: repo/sidecar:dev
+        """.trimIndent(),
+        after = """
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod1
+              annotations:
+                mycompany.io/annotation: "hasvalue"
+            ---
+            apiVersion: v1
+            kind: Pod
+            metadata:
+              name: mypod2
+              ~~(invalid:has(.*))~~>annotations:
+                mycompany.io/annotation: "novalue"
+            ---
+            apiVersion: apps/v1
+            kind: Deployment
+            spec:
+                template:
+                    spec:
+                        metadata:
+                            annotations:
+                                mycompany.io/annotation: "hasvalue"
+                        containers:            
+                        - name: app
+                          image: repo/app:latest
+                        - name: sidecar
+                          image: repo/sidecar:dev
+        """.trimIndent()
+    )
+
+}
