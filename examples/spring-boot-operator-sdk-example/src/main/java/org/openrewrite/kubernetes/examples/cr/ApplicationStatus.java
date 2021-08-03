10309d8388878c39571b76faf7e210912b0668c0
Jon Brisbin <jon@moderne.io>
2021-08-03 15:30:23 -0500

    Add Kubernetes Operator example

---
+++ examples/spring-boot-operator-sdk-example/src/main/java/org/openrewrite/kubernetes/examples/cr/ApplicationStatus.java
@@ -0,0 +1,24 @@
+package org.openrewrite.kubernetes.examples.cr;
+
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.fabric8.kubernetes.api.model.ObjectReference;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ApplicationStatus {
+
+    private final List<ObjectReference> resources;
+
+    @JsonCreator
+    public ApplicationStatus(@Nullable @JsonProperty("resources") List<ObjectReference> resources) {
+        this.resources = resources;
+    }
+
+    public List<ObjectReference> getResources() {
+        return resources;
+    }
+
+}
